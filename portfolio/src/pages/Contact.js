3c904e4ff5ce798a933ef2640f81216fae597d24
Keerthiprasanth <rkpkeerthi22@gmail.com>
2024-02-02 01:37:41 +0000

    Updated home

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div>
+    <div className='contact_content'>
       <h1>Contact Me</h1>
       <h2>Get in touch</h2>
       <h3>Email: rkpkeerthi22@gmail.com</h3>
--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -4,13 +4,13 @@ import './Home.css';
 function Home() {
   return (
     <div className='home'>
+      <div className='home_content'>
       <h1>Hello!</h1>
       <div>I am Keerthi Prasanth Ravichandran, a software engineer specializing in MERN Stack and Android Studio.</div>
       <br />
-      <div className='image'>
-        <img src='../images/animated_software-engineer.png' alt='My_Photo'/>
-        <div class="background-image">Image</div>
       </div>
+      <div class="background-image"></div>
+
     </div>
   )
 }
